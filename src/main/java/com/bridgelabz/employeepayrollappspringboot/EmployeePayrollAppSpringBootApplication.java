8b491fd4a66f64b034037171968b74ac2fb241c4
rulessbroker <rulessbroker>
2023-01-30 15:59:06 +0530

    Added: packages and dependency in pom file

---
+++ src/main/java/com/bridgelabz/employeepayrollappspringboot/EmployeePayrollAppSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.employeepayrollappspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeePayrollAppSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeePayrollAppSpringBootApplication.class, args);
+	}
+
+}
